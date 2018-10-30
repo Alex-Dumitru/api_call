97ba7152f3a978ac4b5ac7f345f40714ad54ace8
Alex Dumitru <Alexandru.Dumitru@ca.com>
2018-10-30 11:56:34 +0100

    added CLI

--- get_word_definition.py
+++ get_word_definition.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import requests
 import json
 import click
@@ -12,7 +15,7 @@ import click
 # responds with JSON or XML - DONE (json)
 # supports GET or POST request type. - DONE (get)
 # format of the program - :$ python script_name.py arg1 arg2 kwarg1=value1 (arguments are just an example).
-# use click for this !!! (should be cooler)
+# use click for this !!! (should be cooler) - NOT OK - use argparse, don't have external dependency
 # allows user to write results to specific file .csv
 # provides support for arguments and keyworded arguments
 # use logging as logger!!! (should be better, and maybe easier to implement the debug flag)
@@ -23,7 +26,7 @@ import click
 # $ python forge_api_script.py output.csv pairs=EURUSD --debug , where:
 
 #     output.csv - output filename
-#     pairs=EURUSD – currency pair to be returned by API
+#     pairs=EURUSD - currency pair to be returned by API
 #     --debug turns debug mode of the script i.e.: more detailed log messages
 
 # Running above command would result in sending request to the following endpoint https://forex.1forge.com/1.0.3/quotes?pairs=EURUSD with pairs=EURUSD as query argument and saving output data to output.csv file.
@@ -64,13 +67,60 @@ import json
 # print("json \n" + json.dumps(r.json()))				
 '''
 
-my_id = 'b9dba52d'
-my_key = '5c6ef63eaa447fc38ec4e0e1c34f8f97'
-language_id = 'en'
-word_id = 'cat'
-# url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word_id.lower()
-url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/%s/%s' % (language_id, word_id.lower())
-r = requests.get(url, headers = {'app_id' : my_id, 'app_key' : my_key})
-print("code {}\n".format(r.status_code))
-print("text \n" + r.text)
-print("json \n" + json.dumps(r.json()))
+class define_word(object):
+    def __init__(self, word):
+        if isinstance(word, (int,float)):
+            raise ValueError
+        try:
+            self.word = str(word)
+        except (NameError, SyntaxError) as err:
+            raise err
+    def get_definition(self):
+        my_id = 'b9dba52d'
+        my_key = '5c6ef63eaa447fc38ec4e0e1c34f8f97'
+        language_id = 'en'
+        word_id = self.word
+        # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word_id.lower()
+        # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/%s/%s' % (language_id, word_id.lower())
+        url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/{lang}/{word}'.format(lang=language_id, word=word_id.lower())
+        print url
+        r = requests.get(url, headers = {'app_id' : my_id, 'app_key' : my_key})
+        print "code {}\n".format(r.status_code)
+        print "text \n" + r.text
+        print "json \n" + json.dumps(r.json())
+
+
+
+
+@click.group()
+@click.option("--debug", is_flag=True, help="print debug messages")
+
+def cli(debug):
+    if debug:
+        # click.echo('Debug mode is %s' % ('on' if debug else 'off'))
+        click.echo('Debug mode is on')
+
+@cli.command()
+@click.option("--word", type=click.STRING, default=None, help="Word to fetch definition for.")
+@click.option("--language", type=click.STRING, default=None, help="Language to be used.")
+def get_definition(word, language):
+    """Get word definition from Oxford Dictionary """
+    cat_definition = define_word('cat')
+    cat_definition.get_definition()
+@cli.command()
+def fake_command():
+    pass
+if __name__ == '__main__':
+    # my_id = 'b9dba52d'
+    # my_key = '5c6ef63eaa447fc38ec4e0e1c34f8f97'
+    # language_id = 'en'
+    # word_id = 'cat'
+    # # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word_id.lower()
+    # # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/%s/%s' % (language_id, word_id.lower())
+    # url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/{lang}/{word}'.format(lang=language_id, word=word_id.lower())
+    # print url
+    # r = requests.get(url, headers = {'app_id' : my_id, 'app_key' : my_key})
+    # print "code {}\n".format(r.status_code)
+    # print "text \n" + r.text
+    # print "json \n" + json.dumps(r.json())
+    cli()
